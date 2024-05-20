045dff6546a0ed6854038783f267919fa4f70e0e
EJPZZZ <ej74519@gmail.com>
2024-05-20 00:15:48 -0600

    Agregados los flujos de carreras y de talleres con sus respectivos "Menus" Y correcion de errores de minusculas y mayusculas

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,6 @@
 import { MemoryDB, addKeyword, createBot, createFlow, createProvider } from '@bot-whatsapp/bot';
 import { BaileysProvider } from '@bot-whatsapp/provider-baileys';
 
-
-
-
 // Función para determinar si es de día o de tarde
 function getSaludo() {
     const horaActual = new Date().getHours();
@@ -16,54 +13,93 @@ function getSaludo() {
     }
 }
 
+// Flujos para cada carrera
+const respuestasCarreras: { [key: string]: string } = {
+    'informatica': 'La carrera de informática trata de computadoras.',
+    'informática': 'La carrera de informática trata de computadoras.',
+    'agronomia': 'Mensaje personalizado para Agronomía.',
+    'agronomía': 'Mensaje personalizado para Agronomía.',
+    'industrial': 'Mensaje personalizado para Industrial.',
+    'energias renovables': 'Mensaje personalizado para Energías Renovables.',
+    'energías renovables': 'Mensaje personalizado para Energías Renovables.',
+    'bioquimica': 'Mensaje personalizado para Bioquímica.',
+    'bioquímica': 'Mensaje personalizado para Bioquímica.',
+    'electromecanica': 'Mensaje personalizado para Electromecánica.',
+    'electromecánica': 'Mensaje personalizado para Electromecánica.',
+    'administracion': 'Mensaje personalizado para Administración.',
+    'administración': 'Mensaje personalizado para Administración.'
+};
+
+// Flujos para talleres
+const respuestasTalleres: { [key: string]: string } = {
+    'ajedrez': 'Mensaje personalizado para el taller de ajedrez.',
+    'basquet': 'Mensaje personalizado para el taller de basquet.',
+    'futbol': 'Mensaje personalizado para el taller de futbol.',
+    'fútbol': 'Mensaje personalizado para el taller de futbol.',
+    'taekwondo': 'Mensaje personalizado para el taller de taekwondo.'
+    // Puedes agregar más talleres y respuestas personalizadas aquí
+};
+
+// Flujo para preguntar sobre la carrera deseada
+const flowInformacionCarreras = addKeyword(['1', 'Info', 'Informacion', 'Información'])
+    .addAnswer('Contamos con 7 carreras:\n1. Informatica\n2. Agronomia\n3. Industrial\n4. Energias renovables\n5. Bioquimica\n6. Electromecanica\n7. Admi\n¿De qué carrera te gustaría información?')
+    .addAnswer('Por favor, escribe el nombre de la carrera.', { capture: true }, async (ctx, {provider}) => {
+        const respuesta = ctx.body.toLowerCase().trim();
+        const respuestaCarrera = respuestasCarreras[respuesta];
+
+        if (respuestaCarrera) {
+            await provider.sendText(ctx.from + '@s.whatsapp.net', respuestaCarrera);
+        } else {
+            await provider.sendText(ctx.from + '@s.whatsapp.net', 'Lo siento, no entendí tu respuesta. Por favor, elige una de las opciones proporcionadas.');
+        }
+    });
+
+// Flujo para talleres
+const flowInformacionTalleres = addKeyword(['2', 'Servicios'])
+    .addAnswer('El ITSS ofrece los siguientes talleres:\n1. Ajedrez\n2. Basquet\n3. Futbol\n4. Taekwondo\n¿De qué taller te gustaría más información? Por favor, escribe el nombre del taller.', { capture: true }, async (ctx, {provider}) => {
+        const respuesta = ctx.body.toLowerCase().trim();
+        const respuestaTaller = respuestasTalleres[respuesta];
+
+        if (respuestaTaller) {
+            await provider.sendText(ctx.from + '@s.whatsapp.net', respuestaTaller);
+        } else {
+            await provider.sendText(ctx.from + '@s.whatsapp.net', 'Lo siento, no entendí tu respuesta. Por favor, elige uno de los talleres proporcionados.');
+        }
+    });
+
 // Flujo de saludo inicial
 const flowSaludoInicial = addKeyword('SaludoInicial')
     .addAnswer(`${getSaludo()}, bienvenido al menú principal. Por favor elige una opción:\n1. Información\n2. Servicios\n3. Contacto\nEscribe el número de la opción deseada.`);
 
 // Flujo del menú principal
-const flowMenu = addKeyword('Menu')
-    .addAnswer('Bienvenido al menú principal. Por favor elige una opción:\n1. Información\n2. Servicios\n3. Contacto\nEscribe el número de la opción deseada.');
+const flowMenu = addKeyword(['Menu','Menú','menú', 'menu'])
+    .addAnswer('Bienvenido al menú principal. Por favor elige una opción:\n1. Información sobre nuestras ingenierias\n2. Talleres\n3. Contactanos\nEscribe el número de la opción deseada.');
 
-// Flujos para cada opción del menú
-const flowInformacion = addKeyword(['1', 'Información'])
-    .addAnswer('Puedo proporcionarte información sobre nuestros servicios. ¿Qué te gustaría saber?');
-
-const flowServicios = addKeyword(['2', 'Servicios'])
-    .addAnswer('Ofrecemos una variedad de servicios, incluyendo:\n1. Asesoría\n2. Soporte técnico\n3. Desarrollo personalizado\n¿Cuál te interesa?');
-
+// Flujos para las otras opciones del menú
 const flowContacto = addKeyword(['3', 'Contacto'])
     .addAnswer('Puedes contactarnos por correo en soporte@ejemplo.com o llamarnos al 123-456-7890.');
 
 // Flujos adicionales
-const flowBienvenida = addKeyword('Hola')
-    .addAnswer('¡Hola! ¿Cómo estás? ¿En qué puedo ayudarte hoy?\nSi necesitas ver el menú principal, escribe "Menu".');
+const flowBienvenida = addKeyword(['Hola', 'hola', 'ola', '.'])
+    .addAnswer('¡Hola! Soy el chat-bot del ITSS🤖 ¿En qué puedo ayudarte hoy?\nSi necesitas ver el menú principal, escribe "Menú".');
 
 const flowAyuda = addKeyword('Ayuda')
     .addAnswer('Claro, estoy aquí para ayudarte. ¿Qué necesitas saber?\nPara ver el menú principal, escribe "Menu".');
 
-const flowAdios = addKeyword('Adios')
+const flowAdios = addKeyword(['Adios','adios', 'adiós', 'Adiós'])
     .addAnswer('Hasta luego, que tengas un buen día.\nPara ver el menú principal en cualquier momento, escribe "Menu".');
 
-// Flujo de fallback para capturar cualquier otro mensaje y mostrar el menú
-const flowFallback = addKeyword('')
-.addAnswer(`${getSaludo()}, bienvenido al menú principal. Por favor elige una opción:\n1. Información\n2. Servicios\n3. Contacto\nEscribe el número de la opción deseada.`);
 
-// Flujo para cuando el usuario no responde después de ver el menú
-const flowNoRespuesta = addKeyword('NoRespuesta')
-    .addAnswer('Lo siento, no has elegido ninguna de las opciones. Por favor, inténtalo de nuevo.');
-
-// Combinar los flujos en un flujo principal
+// Combinar todos los flujos en un flujo principal
 const mainFlow = createFlow([
+    flowInformacionCarreras,
+    flowInformacionTalleres,
     flowSaludoInicial,
+    flowMenu,
+    flowContacto,
     flowBienvenida,
     flowAyuda,
-    flowAdios,
-    flowMenu,
-    flowInformacion,
-    flowServicios,
-    flowContacto,
-    flowNoRespuesta, // Nuevo flujo para capturar cuando no hay respuesta después del menú
-    flowFallback  // El flujo de fallback debe ir al final para capturar cualquier otro mensaje
+    flowAdios
 ]);
 
 // Función principal para inicializar el bot
